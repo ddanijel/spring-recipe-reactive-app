72dc76905a3638fe5bfc0c8d0ad2f2e619ce6b87
danijel <danijel.fon@gmail.com>
2018-08-23 15:45:37 +0200

    init commit

---
+++ src/main/java/itdany/springframework/recipe/app/SpringRecipeMongodbAppApplication.java
@@ -0,0 +1,12 @@
+package itdany.springframework.recipe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeMongodbAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeMongodbAppApplication.class, args);
+    }
+}
