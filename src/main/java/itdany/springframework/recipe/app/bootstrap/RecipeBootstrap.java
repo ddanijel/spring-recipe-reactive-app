afd85d92d4ba44eb41ddf840871d732226819765
danijel <danijel.fon@gmail.com>
2018-08-24 17:40:51 +0200

    Integration Test

--- src/main/java/itdany/springframework/recipe/app/bootstrap/RecipeBootstrap.java
+++ src/main/java/itdany/springframework/recipe/app/bootstrap/RecipeBootstrap.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 @Slf4j
 @Component
-class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
     private final RecipeRepository recipeRepository;
--- src/test/java/itdany/springframework/recipe/app/repository/UnitOfMeasureRepositoryTest.java
+++ src/test/java/itdany/springframework/recipe/app/repository/UnitOfMeasureRepositoryTest.java
@@ -1,28 +1,40 @@
 package itdany.springframework.recipe.app.repository;
 
+import itdany.springframework.recipe.app.bootstrap.RecipeBootstrap;
 import itdany.springframework.recipe.app.model.UnitOfMeasure;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
-@Ignore
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryTest {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
     @Before
     public void setUp() throws Exception {
+        unitOfMeasureRepository.deleteAll();
+        categoryRepository.deleteAll();
+        recipeRepository.deleteAll();
+
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(
+                categoryRepository, recipeRepository, unitOfMeasureRepository);
+        recipeBootstrap.onApplicationEvent(null);
     }
 
     @Test
